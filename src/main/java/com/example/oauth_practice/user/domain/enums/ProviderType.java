49dd056788737a5a7cc8bba7841c3d36e0586059
troy96475 <taesupark0504@gmail.com>
2025-01-24 15:58:04 +0900

    refactor :: providerType

--- src/main/java/com/example/oauth_practice/user/domain/enums/ProviderType.java
+++ src/main/java/com/example/oauth_practice/user/domain/enums/ProviderType.java
@@ -1,14 +1,14 @@
 package com.example.oauth_practice.user.domain.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum ProviderType {
-    LOCAL,
-    GOOGLE
+
+    Local("local"),
+    Google("google");
 
     private final String registrationId;
 
