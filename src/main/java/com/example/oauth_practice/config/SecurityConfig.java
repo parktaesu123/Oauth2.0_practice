587eba37ac51848103f6fa3a5361546bd43f34c2
troy96475 <taesupark0504@gmail.com>
2025-01-24 17:34:20 +0900

    feat :: session cors 설정

--- src/main/java/com/example/oauth_practice/config/SecurityConfig.java
+++ src/main/java/com/example/oauth_practice/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,11 +37,17 @@ public class SecurityConfig {
 
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login") //로그인 페이지 경로
-                        .defaultSuccessUrl("/home") // 로그인 성공 시 리디렉션 경로
-                        .failureUrl("/login?error=true") // 로그인 실패 시 리디렉션 경로
+                        .defaultSuccessUrl("/home", true) // 로그인 성공 경로
+                        .failureUrl("/login?error=true") // 로그인 실패 경로
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuth2UserService)
                         )
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1) //중복 제한
+                        .expiredUrl("/login?expired=true") // 세션만료 경로
                 );
 
         return httpSecurity.build();
