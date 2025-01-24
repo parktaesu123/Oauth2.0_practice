a8a22e1bfe261bc7f918e3aab9709ede7cbca0f4
troy96475 <taesupark0504@gmail.com>
2025-01-24 16:01:25 +0900

    refactor :: 인증관련 예외

--- src/main/java/com/example/oauth_practice/exception/Oauth2AuthenticationException.java
+++ src/main/java/com/example/oauth_practice/exception/Oauth2AuthenticationException.java
@@ -3,8 +3,14 @@ package com.example.oauth_practice.exception;
 import org.springframework.security.core.AuthenticationException;
 
 public class Oauth2AuthenticationException extends AuthenticationException {
-    public OAuth2AuthenticationProcessingException(String msg) {
-        super(msg);
+
+    public Oauth2AuthenticationException(String msg) {
+        super(msg, null);
     }
+
+    public Oauth2AuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
 
 }
