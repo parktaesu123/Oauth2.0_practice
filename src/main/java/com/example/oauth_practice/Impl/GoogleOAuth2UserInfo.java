5ef01b3e12350b05d9ff991bf0b27eee075b5a03
troy96475 <taesupark0504@gmail.com>
2025-01-24 15:25:02 +0900

    feat :: google Oauth 구현체

---
+++ src/main/java/com/example/oauth_practice/Impl/GoogleOAuth2UserInfo.java
@@ -0,0 +1,59 @@
+package com.example.oauth_practice.Impl;
+
+import com.example.oauth_practice.user.domain.enums.ProviderType;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo implements Oauth2UserInfo {
+    private final Map<String, Object> attributes;
+    private final String accessToken;
+    private final String id;
+    private final String email;
+    private final String name;
+    private final String profileImageUrl;
+
+    public GoogleOAuth2UserInfo(String accessToken, Map<String, Object> attributes) {
+        this.accessToken = accessToken;
+        this.attributes = attributes;
+        this.id = (String) attributes.get("id");
+        this.email = (String) attributes.get("email");
+        this.name = (String) attributes.get("name");
+        this.profileImageUrl = (String) attributes.get("picture");
+    }
+
+
+    @Override
+    public ProviderType getProvider() {
+        return ProviderType.GOOGLE;
+    }
+
+    @Override
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+}
