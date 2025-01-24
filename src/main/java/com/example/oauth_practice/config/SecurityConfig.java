73adb26b9a154e3615d884bb0e411668be3f2c4b
troy96475 <taesupark0504@gmail.com>
2025-01-24 15:23:49 +0900

    feat :: cors설정

---
+++ src/main/java/com/example/oauth_practice/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.oauth_practice.config;
+
+import com.example.oauth_practice.application.CustomOauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOauth2UserService customOAuth2UserService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login") //로그인 페이지 경로
+                        .defaultSuccessUrl("/home") // 로그인 성공 시 리디렉션 경로
+                        .failureUrl("/login?error=true") // 로그인 실패 시 리디렉션 경로
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
+
+        return httpSecurity.build();
+    }
+
+}
