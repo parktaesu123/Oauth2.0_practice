ade5819b8af525871038d393b1d39068b4877971
troy96475 <taesupark0504@gmail.com>
2025-01-24 18:14:24 +0900

    refactor :: 클래스 이름 변경

--- src/main/java/com/example/oauth_practice/user/presentation/LoginTestController.java
+++ src/main/java/com/example/oauth_practice/user/presentation/LoginTestController.java
@@ -1,14 +1,19 @@
 package com.example.oauth_practice.user.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class LoginTestController {
     @GetMapping("/login")
-    public String loginPage() {
-        return "login"; // login.html 템플릿을 반환
+    public String loginPage(@RequestParam(required = false) String error) {
+        if (error != null) {
+            // 로그인 실패 처리
+        }
+        return "login"; // login.html 페이지 리턴
     }
+
 
     @GetMapping("/home")
     public String homePage() {
