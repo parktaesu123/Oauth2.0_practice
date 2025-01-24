00242cf6d39e653e99c5140d6490b56d2d2f7ffa
troy96475 <taesupark0504@gmail.com>
2025-01-24 17:33:58 +0900

    feat :: user dao

---
+++ src/main/java/com/example/oauth_practice/user/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.oauth_practice.user.domain.repository;
+
+import com.example.oauth_practice.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
